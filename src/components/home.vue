827e8efa952251b6ed81f359c5616efcfdc7aca4
tbpn21 <1850022494@qq.com>
2020-06-12 21:59:58 +0800

    实现了基本功能，缺少动画效果，方块颜色不能根据分数变化

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,61 +2,61 @@
 <template>
     <div class="main">
         <div class="top">
-            <Button >重玩</Button>&nbsp;&nbsp;
-            <span>分数</span>
+            <Button @click=" restart()">重玩</Button>&nbsp;&nbsp;
+            <span>分数:{{ conunta }}</span>
         </div>
 
         <div class="back">
             <div class="m" id="m-0-0">
-                <div class="moudel" v-show="a00">{{a00}}</div>
+                <div class="moudel" v-show="a00" id="acc">{{a00}}</div>
             </div>
             <div class="m" id="m-0-1">
-                <div class="moudel" v-show="a01">{{a01}}</div>
+                <div class="moudel" v-show="a01" id="acc">{{a01}}</div>
             </div>
             <div class="m" id="m-0-2">
-                <div class="moudel" v-show="a02">{{a02}}</div>
+                <div class="moudel" v-show="a02" id="acc">{{a02}}</div>
             </div>
             <div class="m" id="m-0-3">
-                <div class="moudel" v-show="a03">{{a03}}</div>
+                <div class="moudel" v-show="a03" id="acc">{{a03}}</div>
             </div>
 
             <div class="m" id="m-1-0">
-                <div class="moudel" v-show="a10">{{a10}}</div>
+                <div class="moudel" v-show="a10" id="acc">{{a10}}</div>
             </div>
             <div class="m" id="m-1-1">
-                <div class="moudel" v-show="a11">{{a11}}</div>
+                <div class="moudel" v-show="a11" id="acc">{{a11}}</div>
             </div>
             <div class="m" id="m-1-2">
-                <div class="moudel" v-show="a12">{{a12}}</div>
+                <div class="moudel" v-show="a12" id="acc">{{a12}}</div>
             </div>
             <div class="m" id="m-1-3">
-                <div class="moudel" v-show="a13">{{a13}}</div>
+                <div class="moudel" v-show="a13" id="acc">{{a13}}</div>
             </div>
 
             <div class="m" id="m-2-0">
-                <div class="moudel" v-show="a20">{{a20}}</div>
+                <div class="moudel" v-show="a20" id="acc">{{a20}}</div>
             </div>
             <div class="m" id="m-2-1">
-                <div class="moudel" v-show="a21">{{a21}}</div>
+                <div class="moudel" v-show="a21" id="acc">{{a21}}</div>
             </div>
             <div class="m" id="m-2-2">
-                <div class="moudel" v-show="a22">{{a22}}</div>
+                <div class="moudel" v-show="a22" id="acc">{{a22}}</div>
             </div>
             <div class="m" id="m-2-3">
-                <div class="moudel" v-show="a23">{{a23}}</div>
+                <div class="moudel" v-show="a23" id="acc">{{a23}}</div>
             </div>
 
             <div class="m" id="m-3-0">
-                <div class="moudel" v-show="a30">{{a30}}</div>
+                <div class="moudel" v-show="a30" id="acc">{{a30}}</div>
             </div>
             <div class="m" id="m-3-1">
-                <div class="moudel" v-show="a31">{{a31}}</div>
+                <div class="moudel" v-show="a31" id="acc">{{a31}}</div>
             </div>
             <div class="m" id="m-3-2">
-                <div class="moudel" v-show="a32">{{a32}}</div>
+                <div class="moudel" v-show="a32" id="acc">{{a32}}</div>
             </div>
             <div class="m" id="m-3-3">
-                <div class="moudel" v-show="a33">{{a33}}</div>
+                <div class="moudel" v-show="a33" id="acc">{{a33}}</div>
             </div>
         </div>
     </div>
@@ -86,25 +86,48 @@
         this["a"+c3+c4]=this.arr[randoms>0.6? 1:0]
         }//产生随机数（2||4）的方法
            },
-
-
-        nulls(){
-            let empty=false;
-            for(let i=0; i<4;i++){
-                for(let j=0; j<4; j++){
-                    if(this["a"+i+j]==""){
-                        empty=true;
-                    }
+            restart() {//刷新重玩
+            for(let i=0;i<4;i++) {
+                for(let j=0;j<4;j++) {
+                    this["a"+i+j]="";
                 }
             }
-            return empty;
-        },//判断是否空格子
+            let c1=(Math.random()*3).toFixed(0);
+            let c2=(Math.random()*3).toFixed(0);
+            let random=Math.random().toFixed(1);;
+            this["a"+c1+c2]=this.arr[random>0.6? 1:0];
+            let c3=(Math.random()*3).toFixed(0);
+            let c4=(Math.random()*3).toFixed(0);
+            let randoms=Math.random().toFixed(1);;
+            if(this["a"+c1+c2]== this["a"+c3+c4])
+            {
+            this.generate();
+            }
+            this["a"+c3+c4]=this.arr[randoms>0.6? 1:0];
+           
+            
+        },
+         
+        
+
+
+        // nulls(){
+        //     let empty=false;
+        //     for(let i=0; i<4;i++){
+        //         for(let j=0; j<4; j++){
+        //             if(this["a"+i+j]==""){
+        //                 empty=true;
+        //             }
+        //         }
+        //     }
+        //     return empty;
+        // },//判断是否空格子
    
 
 
         rightUP(){
-             let empty=false;
-            if(this.nulls()) {
+            let flag=false
+           
                 for(let i=0;i<4;i++) {
                     for(let j=2;j>-1;j--) {
                         for(let k=3;k>j;k--) {//k为落子点坐标
@@ -112,22 +135,25 @@
                                 if(this["a"+i+k]==""&&this.xaxis(i,j,k)) {//如果落子点为空的格子并且中间没障碍物则可落子
                                     this["a"+i+k]=this["a"+i+j];//格子赋值
                                     this["a"+i+j]="";
+                                    flag=true
+
                                 } else if(this["a"+i+k]==this["a"+i+j]&&this.xaxis(i,j,k)) {
                                     let a=parseInt(this["a"+i+k])+parseInt(this["a"+i+j]);
                                     this["a"+i+k]=a;
                                     this["a"+i+j]="";
+                                    flag=true
+
                                 }
                             }
                         }
                     }
 
-                }
-            } return empty;
+                } return flag
+             
         },
+
         leftUP(){
-       
-           let empty=false;
-            if(this.nulls()) {
+       let flag=false
                 for(let i=0;i<4;i++) {
                     for(let j=1;j<4;j++) {
                         for(let k=0;k<j;k++) {//k为落子点坐标
@@ -135,25 +161,74 @@
                                 if(this["a"+i+k]==""&&this.xaxis(i,k,j)) {//如果落子点为空的格子并且中间没障碍物则可落子
                                     this["a"+i+k]=this["a"+i+j];//格子赋值
                                     this["a"+i+j]="";
+                                    flag=true
+           
                                 } else if(this["a"+i+k]==this["a"+i+j]&&this.xaxis(i,k,j)) {
                                     let a=parseInt(this["a"+i+k])+parseInt(this["a"+i+j]);
                                     this["a"+i+k]=a;
                                     this["a"+i+j]="";
+                                    flag=true
+
                                 }
                             }
                         }
                     }
 
-                }
-            } return empty;
+                } return flag
+           
         },
         topUP(){
-           this.create(); 
+            let flag=false
+           
+                for(let i=0;i<4;i++) {
+                    for(let j=1;j<4;j++) {
+                        for(let k=0;k<j;k++) {//k为落子点坐标
+                            if(this["a"+j+i]) {//如果要跳转的格子为空 那就跳过
+                                if(this["a"+k+i]==""&&this.yaxis(i,k,j)) {//如果落子点为空的格子并且中间没障碍物则可落子
+                                    this["a"+k+i]=this["a"+j+i];//格子赋值
+                                    this["a"+j+i]="";
+                                    flag=true
+
+                                } else if(this["a"+k+i]==this["a"+j+i]&&this.yaxis(i,k,j)) {
+                                    let a=parseInt(this["a"+k+i])+parseInt(this["a"+j+i]);
+                                    this["a"+k+i]=a;
+                                    this["a"+j+i]="";
+                                    flag=true
+
+                                }
+                            }
+                        }
+                    }
+
+                } return flag
+            
 
 
         },
         downUP(){
-           this.create(); 
+            let flag=false
+                for(let i=0;i<4;i++) {
+                    for(let j=2;j>-1;j--) {
+                        for(let k=3;k>j;k--) {//k为落子点坐标
+                            if(this["a"+j+i]) {//如果要跳转的格子为空 那就跳过
+                                if(this["a"+k+i]==""&&this.yaxis(i,j,k)) {//如果落子点为空的格子并且中间没障碍物则可落子
+                                    this["a"+k+i]=this["a"+j+i];//格子赋值
+                                    this["a"+j+i]="";
+                                    flag=true
+
+                                } else if(this["a"+k+i]==this["a"+j+i]&&this.yaxis(i,j,k)) {
+                                    let a=parseInt(this["a"+k+i])+parseInt(this["a"+j+i]);
+                                    this["a"+k+i]=a;
+                                    this["a"+j+i]="";
+                                    flag=true
+
+                                }
+                            }
+                        }
+                    }
+
+                } return flag
+          
 
 
         },
@@ -164,18 +239,22 @@
                
                 switch(e.keyCode){
                     case 37:
-                       _this.leftUP()
-                        _this.create(); 
+                    if(_this.leftUP()){
+                        _this.create(); }
                         break;
                     case 38:
-                         _this.topUP()  
+                    if(_this.topUP())
+                    {_this.create();}
                         break;
                     case 39:
-                         _this.rightUP()
-                        _this.create(); 
+                    if(_this.rightUP()){
+                        _this.create();
+                    } 
                         break;
                     case 40:
-                        _this.downUP()
+                    if(_this.downUP()){
+                        _this.create(); 
+                    }
                         break;
 
                     }
@@ -188,13 +267,16 @@
             for(var i=y+1;i<z;i++)
                 if(this["a"+x+i])
                     return false;
-            return true;
+                    return true;
+            
         },
         yaxis(x,y,z) {//判断y轴中间是否有障碍物
             for(var i=y+1;i<z;i++)
                 if(this["a"+i+x])
-                    return false;
-            return true;
+                    return false
+                 return true;
+                    
+           
         
     },
 
@@ -215,6 +297,7 @@
          
         
        },
+       
 
 
 
@@ -222,10 +305,11 @@
        }
        
        
-       
+ 
 
 
-            
+
+     
             
 
 
@@ -234,5 +318,9 @@
 <style>
 
  @import "../assets/css/backg.css";
+ 
+     
+
+
 </style>
 
